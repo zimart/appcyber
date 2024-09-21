eb79e6afb9310440bccfe3e28d027f2a94926915
Artur Zimoń <a.zimon@sigma.sanok.pl>
2024-09-21 18:20:46 +0000

    test3d

--- pages/Screeny_inventory/jsobjects/powierzchnian/powierzchnian.js
+++ pages/Screeny_inventory/jsobjects/powierzchnian/powierzchnian.js
@@ -2,11 +2,13 @@ export default {
 	
 m2: function () {
 var szerokoscm2 = szerokosc_input.text;
-var wysokoscm2 = podawana_wysokosc_input.text;
-var powierzchniam2 = (szerokoscm2*wysokoscm2)/100000;
+//var wysokoscm2 = wysokosc_input.text;
+var wysokoscm2 = appsmith.store.calkowita;	
+var powierzchniam2 = (szerokoscm2*wysokoscm2)/1000000;
 
 //return 	powierzchniam2.toFixed(2);
 return Number.parseFloat(powierzchniam2).toFixed(2);	
+//	return powierzchniam2
 },	
 	
 	 
