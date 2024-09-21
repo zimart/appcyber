20d1b2cc6ec6e14b8a8afe990872f010706e0663
Artur Zimoń <a.zimon@sigma.sanok.pl>
2024-09-21 19:56:53 +0000

    21-09-2

--- pages/Screeny_inventory/jsobjects/wysokosccala/wysokosccala.js
+++ pages/Screeny_inventory/jsobjects/wysokosccala/wysokosccala.js
@@ -3,10 +3,10 @@ export default {
 wys2: function () {
 var szerokoscm = rodzajwysokosci.selectedOptionValue;
 var podana = podawana_wysokosc_input.text;
-var skrzynka = Select8.selectedOptionValue;
+var skrzynka = wartoscskrzynka.text;
 var szerm33;
 if (szerokoscm === '1') { szerm33 = podana; }
-else if (szerokoscm === '2') { szerm33 = podana ; }
+else if (szerokoscm === '2') { szerm33 = podana + skrzynka }
 return szerm33;
 },
 	
