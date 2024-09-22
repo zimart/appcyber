7a4767a288c8dfb35f565bf6bd429674139055d4
Artur Zimoń <a.zimon@sigma.sanok.pl>
2024-09-22 10:34:12 +0000

    22-09-12-34

--- pages/Screeny_inventory/jsobjects/doplata/doplata.js
+++ pages/Screeny_inventory/jsobjects/doplata/doplata.js
@@ -11,7 +11,15 @@ var doplatakolor = (cenasystemubaza*(doplatakolorinput.text/100));
 return doplatakolor.toFixed(2);
 },
 	
-	
+tkanina: function () {
+var kosztmetra = doplatatkaninam2.text;
+var powierzchniam2 = powierzchnia.text
+//var skrzynka = Select8.selectedOptionValue;
+var doplatatkanina = (kosztmetra * powierzchniam2);
+//if (szerokoscm === '1') { szerm33 = podana; }
+//else if (szerokoscm === '2') { szerm33 = podana ; }
+return doplatatkanina.toFixed(2);
+},
 	 
 	
 };
