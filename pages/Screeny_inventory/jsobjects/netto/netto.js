8e3801da55d1971adbc9d37d164f5ee43f77b59e
Artur Zimoń <a.zimon@sigma.sanok.pl>
2024-09-22 19:31:00 +0000

    22-09-21-30

---
+++ pages/Screeny_inventory/jsobjects/netto/netto.js
@@ -0,0 +1,13 @@
+export default {
+	
+netto: function () {
+	
+var cenasystemu = currentRow.cena;
+var doplatakolor = currentRow.doplatatkanina;
+var cenanetto = cenasystemu + doplatakolor;
+return cenanetto.toFixed(2);
+},
+	
+
+	
+};
